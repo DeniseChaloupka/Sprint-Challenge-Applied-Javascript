cb943bbc41460afa41fe540888bf31fad3775c8d
Denise Chaloupka <denisechaloupka@yahoo.com>
2019-07-19 21:28:14 -0500

    finished tabs -mvp

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,10 +7,26 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
-axios.get('https://lambda-times-backend.herokuapp.com/topics')
-    .then(response => {
-        console.log(response.data)
+axios
+    .get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(res => {
+        const topics = document.querySelector(".topics");
+        return res.data.topics.map(topic => {
+            return topics.appendChild(Tab(topic));
+        });
     })
-    .catch(err => {
-        console.log(err)
-    })
+
+    .catch(error => {
+        console.log("error message", error);
+    });
+
+function Tab(topic) {
+
+    const tab = document.createElement("div");
+
+    tab.classList.add("tab");
+
+    tab.textContent = topic;
+
+    return tab
+}
