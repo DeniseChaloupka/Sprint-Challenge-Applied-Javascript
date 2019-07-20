9a363ba04b3d2b43f3d8ac9846acda77b547d7ae
Denise Chaloupka <denisechaloupka@yahoo.com>
2019-07-19 21:47:11 -0500

    completed cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,45 +18,103 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 
-const cardcontainer = document.querySelector(".cards-container");
+const cardsContainer = document.querySelector('.cards-container');
+// console.log(cardsContainer);
 
-axios.get("https://lambda-times-backend.herokuapp.com/articles")
-    .then(res => {
-        const articles = res.data.articles; // Object
-        const articleTopic = Object.keys(articles); // Returns an array of all of the object keys
-
-        articleTopic.map(topic => {
-            articles[`${topic}`].map(article => {
-                cardcontainer.append(createarticlecards(article));
-            });
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(data => {
+        // console.log('data', data);
+        const articleArray = data.data.articles.javascript;
+        // console.log('JS Articles', JSArray);
+        const cardsContainer = document.querySelector('.cards-container');
+        articleArray.forEach((article) => {
+            const newArticle = articleCreator(article);
+            newArticle.setAttribute('data-card', 'javascript');
+            cardsContainer.appendChild(newArticle);
         });
-    })
-    .catch(err => {
-        console.log("error message 2 ", err);
     });
 
-function createarticlecards(data) {
-    const card = document.createElement("div");
-    const headline = document.createElement("div");
-    const authordiv = document.createElement("div");
-    const imgdiv = document.createElement("div");
-    const img = document.createElement("img");
-    const author = document.createElement("span");
 
-    card.classList.add("card");
-    headline.classList.add("headline");
-    authordiv.classList.add("author");
-    imgdiv.classList.add("img-container");
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(data => {
+        const articleArray = data.data.articles.bootstrap;
+        const cardsContainer = document.querySelector('.cards-container');
+        articleArray.forEach((article) => {
+            const newArticle = articleCreator(article);
+            newArticle.setAttribute('data-card', 'bootstrap');
+            cardsContainer.appendChild(newArticle);
+        });
+    });
 
-    headline.textContent = data.headline;
-    img.src = data.authorPhoto;
-    author.textContent = data.authorName;
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(data => {
+        const articleArray = data.data.articles.bootstrap;
+        const cardsContainer = document.querySelector('.cards-container');
+        articleArray.forEach((article) => {
+            const newArticle = articleCreator(article);
+            newArticle.setAttribute('data-card', 'bootstrap');
+            cardsContainer.appendChild(newArticle);
+        });
+    });
 
-    card.append(headline);
-    card.append(authordiv);
-    authordiv.append(imgdiv);
-    authordiv.append(author);
-    imgdiv.append(img);
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(data => {
+        const articleArray = data.data.articles.technology;
+        const cardsContainer = document.querySelector('.cards-container');
+        articleArray.forEach((article) => {
+            const newArticle = articleCreator(article);
+            newArticle.setAttribute('data-card', 'technology');
+            cardsContainer.appendChild(newArticle);
+        });
+    });
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(data => {
+        const articleArray = data.data.articles.jquery;
+        const cardsContainer = document.querySelector('.cards-container');
+        articleArray.forEach((article) => {
+            const newArticle = articleCreator(article);
+            newArticle.setAttribute('data-card', 'jquery');
+            cardsContainer.appendChild(newArticle);
+        });
+    });
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(data => {
+        const articleArray = data.data.articles.node;
+        const cardsContainer = document.querySelector('.cards-container');
+        articleArray.forEach((article) => {
+            const newArticle = articleCreator(article);
+            newArticle.setAttribute('data-card', 'node.js');
+            cardsContainer.appendChild(newArticle);
+        });
+    });
+
+function articleCreator(article) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const headline = document.createElement('div');
+    headline.classList.add('headline');
+    headline.textContent = article.headline;
+
+    const author = document.createElement('div');
+    author.classList.add('author');
+
+    const img = document.createElement('img');
+    img.classList.add('img-container');
+    img.src = article.authorPhoto;
+
+    const by = document.createElement('span');
+    by.textContent = article.authorName;
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(img);
+    author.appendChild(by);
 
     return card;
 }
