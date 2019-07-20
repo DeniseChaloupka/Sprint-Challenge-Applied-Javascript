6fc71eb22405c8c5a56be12d2a8457cb3728ab31
Denise Chaloupka <denisechaloupka@yahoo.com>
2019-07-19 21:29:05 -0500

    working on header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,13 +9,33 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {
-    const header = document.createElement('div');
-    const date = document.createElement('span');
-    const title = document.createElement('h1');
-    const temperature = document.createElement('span');
-    header.classList.add('header');
-    date.classList.add('date');
-    temperature.classList.add('temp');
+const pageheader = document.querySelector('.header-container');
+pageheader.appendChild(Header());
 
+
+function Header() {
+
+    //elements
+    const header = document.createElement("div");
+    const headerdate = document.createElement("span");
+    const header1 = document.createElement("h1");
+    const headertemp = document.createElement("span");
+
+    //styles
+    header.classList.add("header");
+    headerdate.classList.add("date");
+    header1.classList.add("h1");
+    headertemp.classList.add("temp");
+
+    //content
+    headerdate.textContent = "SMARCH 28, 2019";
+    header1.textContent = "Lambda Times";
+    headertemp.textContent = "98°";
+
+    //append
+    header.appendChild(headerdate);
+    header.appendChild(header1);
+    header.appendChild(headertemp);
+
+    return header;
 }
